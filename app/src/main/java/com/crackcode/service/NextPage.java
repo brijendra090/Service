b84afe438c3107ec1581ea5d8dbe09d72532ee54
brijendra090 <ibmbrijendra93@gmail.com>
2019-09-11 02:02:41 +0530

    Initial commit

---
+++ app/src/main/java/com/crackcode/service/NextPage.java
@@ -0,0 +1,15 @@
+package com.crackcode.service;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NextPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_page);
+    }
+}
